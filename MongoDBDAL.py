4305ab5b967be3eee138e36594778da2dec18c98
Yaniv <hadary@gmail.com>
2022-04-16 23:07:38 +0300

    better crud in mongo

--- MongoDBDAL.py
+++ MongoDBDAL.py
@@ -1,4 +1,33 @@
+import os
+
 import pymongo
+import gridfs
+
 class MongoDBDAL:
-    def __init__(self):
-        self.client= pymongo.MongoClient("localhost", 27017)
+
+    def __init__(self,ip,port,db_name):
+        self.client= pymongo.MongoClient(ip, port)
+        self.database = self.client[db_name]
+        # Create an object of GridFs for the above database.
+        self.fs = gridfs.GridFS(self.database)
+
+    def insert_image_file(self,file_name,movie_name,imdb_code):
+        file = file_name
+        head, tail = os.path.split(file_name)
+        with open(file, 'rb') as f:
+            current_image = f.read()
+        self.fs.put(current_image, file_name=tail,movie_name=movie_name,imdb_code=imdb_code)
+
+    def read_image_file(self,movie_name):
+        file = self.fs.find_one({"movie_name": movie_name}).read()
+        with open(movie_name+".jpeg", 'wb') as w:
+            w.write(file)
+
+
+if __name__ == "__main__":
+    mdb=MongoDBDAL("localhost",27017,"movies")
+    content_temp_path="./temp_content/"
+
+    mdb.insert_image_file(content_temp_path+"poster_spiderman.jpeg","spiderman","tt0145487")
+    mdb.read_image_file("spiderman")
+
--- TMDBDownLoader.py
+++ TMDBDownLoader.py
@@ -2,62 +2,52 @@ import requests
 from config import TMDB_API_Key_v3_auth
 import imdb
 
-def size_str_to_int(x:str)->int:
+
+def size_str_to_int(x: str) -> int:
     """sorting function to get the biggest picture size """
     return float("inf") if x == 'original' else int(x[1:])
 
+
 class TMDBDownloader:
+    content_temp_path="./temp_content/"
     def __init__(self):
         self.CONFIG_PATTERN = 'http://api.themoviedb.org/3/configuration?api_key={key}'
         self.base_url = 'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/'
-
-        self.IMG_PATTERN = ''
+        self.IMG_PATTERN = 'http://api.themoviedb.org/3/movie/{imdbid}/images?api_key={key}'
         self.KEY = TMDB_API_Key_v3_auth
         self.url = self.CONFIG_PATTERN.format(key=self.KEY)
         self.config = requests.get(self.url).json()
-        #print(self.config)
+
         self.base_url = self.config['images']['base_url']
         self.sizes = self.config['images']['poster_sizes']
-        self.max_size = max(self.sizes, key=size_str_to_int) #use the sort function in max to get biggest size
-    def getIMDBID(self,name):
-        # creating instance of IMDb
+        self.max_size = max(self.sizes, key=size_str_to_int)  # use the sort function in max to get biggest size
+
+    def getIMDBID(self, name):
         ia = imdb.IMDb()
         search = ia.search_movie(name)
-        return "tt"+str(search[0].movieID)
+        return "tt" + str(search[0].movieID)
 
-    def getPoster(self,imdbid):
-        self.IMG_PATTERN = 'http://api.themoviedb.org/3/movie/{imdbid}/images?api_key={key}'
-
-        api_response =  requests.get(self.IMG_PATTERN.format(key=self.KEY, imdbid=imdbid)).json()
+    def getPoster(self, imdbid, name):
+        api_response = requests.get(self.IMG_PATTERN.format(key=self.KEY, imdbid=imdbid)).json()
         posters = api_response['posters']
         poster_urls = []
         for poster in posters:
             rel_path = poster['file_path']
             url = "{0}{1}{2}".format(self.base_url, self.max_size, rel_path)
             poster_urls.append(url)
-#########single poster dowload
-        print(poster_urls)
+        # single poster download
+
         r = requests.get(poster_urls[0])
-        nr=0
         filetype = r.headers['content-type'].split('/')[-1]
-        filename = 'poster_{0}.{1}'.format(nr + 1, filetype)
-        with open(filename, 'wb') as w:
+        filename = 'poster_{0}.{1}'.format(name, filetype)
+        with open(self.content_temp_path+filename, 'wb') as w:
             w.write(r.content)
 
-        # for nr, url in enumerate(poster_urls):
-        #     r = requests.get(url)
-        #     filetype = r.headers['content-type'].split('/')[-1]
-        #     filename = 'poster_{0}.{1}'.format(nr + 1, filetype)
-        #     with open(filename, 'wb') as w:
-        #         w.write(r.content)
-        return poster_urls
+        return
 
 
 if __name__ == "__main__":
-    TMDBconn=TMDBDownloader()
-    imdb_id=TMDBconn.getIMDBID("avengers")
-    print(imdb_id)
-    response=TMDBconn.getPoster(imdb_id)
-    #print(response)
-
-    print("hi")
+    TMDBconn = TMDBDownloader()
+    movie_name="spiderman"
+    imdb_id = TMDBconn.getIMDBID(movie_name)
+    response = TMDBconn.getPoster(imdb_id,movie_name)
