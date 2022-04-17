f32c6a58d03406d788f72cfdd4437022b2841d1e
Yaniv <hadary@gmail.com>
2022-04-17 10:33:35 +0300

    added comments for mongo class

--- MongoDBDAL.py
+++ MongoDBDAL.py
@@ -1,16 +1,27 @@
 import os
 import pymongo
 import gridfs
-import config
+import config #this file conteins the key - shouldnt be in git, find automation sol for secrets
 
 class MongoDBDAL:
+    """
+    Data Access Layer for mongodb
+    in order to save big files we need to use GridFS
+    mongodb solution for large binary files
+    """
     def __init__(self, ip, port, db_name):
+        """"creating client and gridfs object"""
         self.client = pymongo.MongoClient(ip, port)
         self.database = self.client[db_name]
         # Create an object of GridFs for the above database.
         self.fs = gridfs.GridFS(self.database)
 
     def write_image_file(self, file_name, movie_name, imdb_code):
+        """
+        add image file to db
+        open image file saved in the FS
+        rec metadata : file name, movie name, imdb code
+        """
         file = file_name
         head, tail = os.path.split(file_name)
         with open(file, 'rb') as f:
@@ -18,17 +29,38 @@ class MongoDBDAL:
         self.fs.put(current_image, file_name=tail, movie_name=movie_name, imdb_code=imdb_code)
 
     def read_image_file(self, movie_name):
+        """
+        search and read file from db
+        saves it in wroking folder
+        """
         file = self.fs.find_one({"movie_name": movie_name}).read()
         with open(movie_name + ".jpeg", 'wb') as w:
             w.write(file)
 
     def search_image_file_id_by_name(self, movie_name):
+        """
+        serach file gridfs id
+        :param movie_name:
+        :return:
+        """
         return self.fs.find_one({"movie_name": movie_name})._id
 
     def del_image_file(self, movie_name):
+        """
+        delete movie poster
+        :param movie_name:
+        :return:
+        """
         self.fs.delete(self.search_image_file_id_by_name((movie_name)))
 
     def update_image_file_meta_data(self,movie_name,key_to_update,val_to_update):
+        """
+        updtae metadata for image
+        :param movie_name:
+        :param key_to_update:
+        :param val_to_update:
+        :return:
+        """
         file_id=self.search_image_file_id_by_name((movie_name))
         mycol = self.database["fs.files"]
         myquery = {"_id": file_id}
@@ -37,6 +69,9 @@ class MongoDBDAL:
 
 
 if __name__ == "__main__":
+    """
+    test module
+    """
     mdb = MongoDBDAL("localhost", 27017, "movies")
 
     mdb.write_image_file(config.content_temp_path + "poster_spiderman.jpeg", "spiderman", "tt0145487")
